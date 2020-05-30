14049a8eb814fd40214e36bb01b946ebf21acd97
chumpon-asaneerat <chumponsenate@gmail.com>
2020-05-30 16:30:08 +0700

    Init .NET Owin Rest api Sample

---
+++ ValuesController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+
+namespace RestAPISelfHostSample
+{
+    /*
+    public class ValuesController : ApiController
+    {
+        // GET api/values 
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5 
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values 
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5 
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5 
+        public void Delete(int id)
+        {
+        }
+    }
+    */
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+
+
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+        };
+
+        public IEnumerable<Product> Get()
+        {
+            return products;
+        }
+
+        public Product Get(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return product;
+        }
+
+        public IEnumerable<Product> Get(string category)
+        {
+            return products.Where(p => string.Equals(p.Category, category,
+                    StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
