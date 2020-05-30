1871b56226b354a0458ad796014f1bd796b485bd
chumpon-asaneerat <chumponsenate@gmail.com>
2020-05-30 16:33:00 +0700

    Add some more data

--- ValuesController.cs
+++ ValuesController.cs
@@ -53,7 +53,8 @@ namespace RestAPISelfHostSample
         {
             new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
             new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
-            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M },
+            new Product { Id = 4, Name = "Driver", Category = "Hardware", Price = 6.12M }
         };
 
         public IEnumerable<Product> Get()
